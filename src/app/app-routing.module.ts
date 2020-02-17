e8502c72fd8605fc2b75979284dd7d5b0edc1375
bkhalifa <benkhalifa.bilel@hotmail.fr>
2020-02-18 00:27:22 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,19 @@ import { HomeComponent } from './home/home.component';
 import { HikeListComponent } from './hike/hike-list.component';
 import {HikeDetailsComponent} from './hike/hike-details.component';
 import { PageNotFound } from "./page-not-found.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AboutComponent } from "./about/about.component";
 
-const routes :any=[
+const routes :any = [
   {path:'home', component:HomeComponent},
   {path:'hikes', component:HikeListComponent} ,
   {path:'hikes/:id', component:HikeDetailsComponent} ,
+  {path:'contact', component:ContactComponent} ,
+  {path:'about', component:AboutComponent} ,
   {path:'', redirectTo:'home',pathMatch:'full'} ,
   {path:'**', component:PageNotFound}
        ];
+
 
 @NgModule({
 imports:[RouterModule.forRoot(routes) ],
