8771c94b6b01edd85a2403483a9605c141bb8d35
bkhalifa <benkhalifa.bilel@hotmail.fr>
2020-02-18 00:34:49 +0100

    routing

--- src/app/app-routing.module.js
+++ src/app/app-routing.module.js
@@ -11,10 +11,14 @@ var home_component_1 = require("./home/home.component");
 var hike_list_component_1 = require("./hike/hike-list.component");
 var hike_details_component_1 = require("./hike/hike-details.component");
 var page_not_found_component_1 = require("./page-not-found.component");
+var contact_component_1 = require("./contact/contact.component");
+var about_component_1 = require("./about/about.component");
 var routes = [
     { path: 'home', component: home_component_1.HomeComponent },
     { path: 'hikes', component: hike_list_component_1.HikeListComponent },
     { path: 'hikes/:id', component: hike_details_component_1.HikeDetailsComponent },
+    { path: 'contact', component: contact_component_1.ContactComponent },
+    { path: 'about', component: about_component_1.AboutComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', component: page_not_found_component_1.PageNotFound }
 ];
